40edaa8177d6a41c8f6435f46cc467d61da0b85a
SamiIzawa <93434132+sami336@users.noreply.github.com>
2022-06-20 12:53:44 +0000

    管理者ログイン画面のリンクを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admin, controllers: {
   sessions:      'admin/sessions',
   }
-  
+
 
   devise_for :members, controllers: {
   sessions:      'public/sessions',
   passwords:     'public/passwords',
   registrations: 'public/registrations'
   }
-  
+
+
   namespace :admin do
     resources :members
+    resources :genres, only: [:index, :edit, :create, :update]
+    root to: 'homes#top'
   end
   scope module: :public do
     resources :members
-  end  
-  
+  end
+
   # devise_for :members
   # devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
