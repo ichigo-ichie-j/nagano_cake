27ba38499af71a0ef4a97800bf088b09dc64122b
ichigo-ichie-j <zebest3824@gmail.com>
2022-06-20 09:14:02 +0000

    会員マイページの作成”

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   namespace :public do
     get 'members/show'
     get 'members/edit'
@@ -10,21 +10,21 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
   sessions:      'admin/sessions',
   }
-  
+
 
   devise_for :members, controllers: {
   sessions:      'public/sessions',
   passwords:     'public/passwords',
   registrations: 'public/registrations'
   }
-  
+
   namespace :admin do
     resources :members
   end
   scope module: :public do
     resources :members
-  end  
-  
+  end
+
   # devise_for :members
   # devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
