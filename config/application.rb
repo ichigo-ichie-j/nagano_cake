ce88473d1576e66f4dbb5fef63a1c675467f4dc0
SamiIzawa <93434132+sami336@users.noreply.github.com>
2022-06-23 12:42:26 +0000

    注文履歴一覧、詳細途中。enum追加

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,13 @@ module NaganoCake
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
+    # i18n
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
   end
 end
