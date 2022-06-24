aac9e9df2782689e1038b981602002134110280b
EC2 Default User <ec2-user@ip-172-31-5-201.ec2.internal>
2022-06-24 05:55:52 +0000

    顧客の商品一覧＆詳細画面作成2

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,14 +1,14 @@
 class Public::ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all.page(params[:page]).per(8)
     # @item = Item.find(params[:id])
   end
-  
+
   def show
     @items = Item.all
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
-  
+
 end
