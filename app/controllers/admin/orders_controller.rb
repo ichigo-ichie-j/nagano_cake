58fdd6925a0a34f7a8b3c79ee9d4515993d9c587
SamiIzawa <93434132+sami336@users.noreply.github.com>
2022-06-27 06:03:49 +0000

    注文・制作ステータスの実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,9 +6,13 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    order = Order.find(params[:id])
-    order.update(order_params)
-    redirect_to admin_order_path(order.id)
+    @order = Order.find(params[:id])
+    order_items = @order.order_items
+    @order.update(order_params)
+    if @order.request_status == "confirm"
+      order_items.update_all(production_status: "wait")
+    end
+    redirect_to admin_order_path(@order.id)
   end
 
 private
