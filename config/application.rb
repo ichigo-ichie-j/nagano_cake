a5c009e30736310c5fe5fd1210cd0f29065da93c
ichigo-ichie-j <zebest3824@gmail.com>
2022-06-28 07:34:11 +0000

    完成コミット

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module NaganoCake
     config.time_zone = 'Asia/Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
 
   end
